0a9b418abaf6d51ba223a265eca982aae1f3091a
Александр Яковлев <alykov@mumkp.com>
2020-03-27 13:14:33 +0700

    Название и перевёл.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,13 +79,13 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    LEFF laravel Testing
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
+                    <a href="https://laravel.com/docs">Документация</a>
                     <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
+                    <a href="https://laravel-news.com">Новости</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
